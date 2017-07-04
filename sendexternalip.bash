3deece73c24091a0276b12e08db04efa4d352630
el-beth <enduad@gmail.com>
2017-07-04 16:04:30 +0300

    Add files via upload

---
+++ sendexternalip.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+## author: Endrias A. Damtie
+## abstract: this script sends your Internet facing external IP to an email address using the lightweight SMTP client "sendemail" to the anonymous gorilla mail service.
+## the default receiver address is receiver@grr.la however if a different gorilla mail address is provided, the script will utilize that address.
+
+senderEmail="sender@endr.ias"
+
+if [ -n "$1" ];
+	then receiverEmail="$1";
+	else receiverEmail="receiver@grr.la";
+fi;
+
+checkPPAs(){
+	if [ -z $(dpkg -l | grep -o "sendemail") ]; 
+		then sendemailavail=0; 
+		else sendemailavail=1; 
+	fi;
+
+	if [ -z $(dpkg -l | grep -o "libio-socket-ssl-perl") ]; 
+		then libioavail=0; 
+		else libioavail=1; 
+	fi;
+
+	if [ -z $(dpkg -l | grep -o "libnet-ssleay-perl") ]; 
+		then libnetavail=0; 
+		else libnetavail=1; 
+	fi;	
+}
+
+installPPAs(){
+	if [ $sendemailavail == '0' ];
+		then apt-get install sendemail;
+	fi;
+
+	if [ $libioavail == '0' ];
+		then apt-get install libio-socket-ssl-perl;
+	fi;
+
+	if [ $libnetavail == '0' ];
+		then apt-get install libnet-ssleay-perl;
+	fi;
+}
+
+getExternalIP(){
+	externalIP=$( dig +short myip.opendns.com @resolver1.opendns.com );
+}
+
+sendExternalIP(){
+	sendEmail -f $senderEmail -t $receiverEmail -u "External IP for $(date)" -m "$externalIP" -s mail.guerrillamail.com:25 ;
+}
+
+main(){
+	checkPPAs;
+	installPPAs;
+	getExternalIP;
+	sendExternalIP;
+}
+
+
+main > /dev/null;
